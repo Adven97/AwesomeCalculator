55a24de8d8d2b9fb8c0e7954c60ca6e9e357bdf3
Adam Tomczak <atomczak30@gmail.com>
2019-03-24 21:04:24 +0100

    nie wyglada ladnie ale dziala bez obracania

---
+++ app/src/main/java/advenstudios/awesomecalkculator/MainActivity.java
@@ -0,0 +1,30 @@
+package advenstudios.awesomecalkculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button simple;
+    Button advanced;
+    Button about;
+    Button exit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        simple = findViewById(R.id.simple);
+
+        simple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ActivitySimpleCalc.class));
+            }
+        });
+
+    }
+}
